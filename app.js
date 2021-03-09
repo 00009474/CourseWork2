973825230c573d4a7ca332d623b8b478e2fc03ae
00009474 <student00009474@gmail.com>
2021-03-09 15:24:14 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = 3000 
+
+// connect to MongoDB
+const dbSource = 'mongodb+srv://student00009474:00009474@todoapp.rfilj.mongodb.net/todo-db?retryWrites=true&w=majority'
+mongoose.connect(dbSource, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then ((result) => console.log('connected to db'))
+    .catch ((err) => console.log(err))
+
+const indexRoutes = require('./routes/index')
+app.use ('/', indexRoutes)
+
+app.use (bodyParser.urlencoded({extended: false}))
+
+app.set('view engine', 'pug')
+
+
+app.listen(port, () => console.log(`Server Started ${port}.` ));
+
---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const getIndex = (req, res) => {
+    res.render('index')
+}
+
+const post_index = (req,res) => {
+    console.log(req.body.title) 
+    res.redirect('/')
+}
+
+module.exports = {
+    getIndex: getIndex,
+    post_index: post_index 
+}
