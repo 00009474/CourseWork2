7d93fcbd192879f3469f3f8a1c556d35df4b6b69
00009474 <student00009474@gmail.com>
2021-03-10 10:09:55 +0500

    first commit

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,16 @@ const getIndex = (req, res) => {
     res.render('index')
 }
 
+const Todo = require('../models/Todo')
+
 const post_index = (req,res) => {
-    console.log(req.body.title) 
-    res.redirect('/')
+    const newTodo = new Todo ({
+        comment: req.body.comment
+    })
+    newTodo.save((err) => {
+        if(err) console.log(err)
+        res.redirect('/')
+    })
 }
 
 module.exports = {
